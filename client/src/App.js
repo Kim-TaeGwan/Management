e4b47b540029f6f39825fadfe634c971ffec50da
kimtaegwan <ab154@naver.com>
2021-03-12 20:28:30 +0900

    Add Get Customers REST Api MOdules

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import React, { Component } from "react";
+
 import {
   Paper,
   Table,
@@ -20,72 +22,58 @@ const styles = theme => ({
   },
 });
 
-const customers = [
-  {
-    id: 1,
-    image: "https://placeimg.com/64/64/1",
-    name: "홍길동",
-    birthday: "961222",
-    gender: "남자",
-    job: "대학생",
-  },
-  {
-    id: 2,
-    image: "https://placeimg.com/64/64/2",
-    name: "고길동",
-    birthday: "960122",
-    gender: "남자",
-    job: "대학생",
-  },
-  {
-    id: 3,
-    image: "https://placeimg.com/64/64/3",
-    name: "황길동",
-    birthday: "960222",
-    gender: "남자",
-    job: "대학생",
-  },
-  {
-    id: 4,
-    image: "https://placeimg.com/64/64/4",
-    name: "test",
-    birthday: "930123",
-    gender: "여자",
-    job: "백수",
-  },
-];
-
-const App = props => {
-  const { classes } = props;
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {customers.map(customer => (
-            <Customer
-              key={customer.id}
-              id={customer.id}
-              image={customer.image}
-              name={customer.name}
-              birthday={customer.birthday}
-              gender={customer.gender}
-              job={customer.job}
-            />
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
-};
+class App extends Component {
+  state = {
+    customers: "",
+  };
+  componentDidMount() {
+    // 모든 컴포넌트가 마운트 되었을떄 실행
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      // callApi의 데이터가 담긴 body를 res에 담는다
+      .catch(err => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch("/api/customers"); // 접속하고자 하는 api의 주소
+    const body = await response.json(); // json형식으로 출력
+    return body;
+  };
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customers
+              ? this.state.customers.map(customer => {
+                  return (
+                    <Customer
+                      key={customer.id}
+                      id={customer.id}
+                      image={customer.image}
+                      name={customer.name}
+                      birthday={customer.birthday}
+                      gender={customer.gender}
+                      job={customer.job}
+                    />
+                  );
+                })
+              : "불러오는중"}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+}
 
 export default withStyles(styles)(App);
