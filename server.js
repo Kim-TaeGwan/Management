8eb6b084bf199c40de1410f7289741ae0a4652a3
kimtaegwan <ab154@naver.com>
2021-03-19 18:18:30 +0900

    Add Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,46 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const fs = require("fs");
+const mysql = require("mysql");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database,
+});
+
+connection.connect();
+
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/1",
-      name: "홍길동",
-      birthday: "961222",
-      gender: "남자",
-      job: "대학생",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/2",
-      name: "고길동짱",
-      birthday: "960122",
-      gender: "남자",
-      job: "대학생",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/3",
-      name: "황길동",
-      birthday: "960222",
-      gender: "남자",
-      job: "대학생",
-    },
-    {
-      id: 4,
-      image: "https://placeimg.com/64/64/4",
-      name: "test",
-      birthday: "930123",
-      gender: "여자",
-      job: "백수",
-    },
-  ]);
+  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
